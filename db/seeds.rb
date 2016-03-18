e6b8c1042171cc892303a7df026b0881c5215fbc
mmzhai1 <mmzhai@berkeley.edu>
2016-03-18 18:34:19 +0000

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-#User.create!(:name => 'Jason Chern', :email => 'jason.chern93@gmail.com', :password => 'password', :role => 'Admin')
-#User.create!(:name => 'Alison Lam', :email => 'alam@collegetrack.org', :password => 'password', :role => 'Admin')
-#User.create!(:name => 'Peter Duong', :email => 'petrduong@gmail.com', :password => 'password', :role => 'Admin')
-#User.create!(:name => 'Edward Huang', :email => 'shinyenhuang@gmail.com', :password => 'password', :role => 'Admin')
-#User.create!(:name => 'Chang Liang', :email => 'changwliang@gmail.com', :password => 'password', :role => 'Admin')
-#User.create!(:name => 'Kyle Chang', :email => 'hchang409@gmail.com', :password => 'password', :role => 'Admin')
-#User.create!(:name => 'Jerry Uejio', :email => 'jerryuejio@gmail.com', :password => 'password', :role => 'Admin')
+User.create!(:name => 'Jason Chern', :email => 'jason.chern93@gmail.com', :password => 'password', :role => 'Admin')
+User.create!(:name => 'Alison Lam', :email => 'alam@collegetrack.org', :password => 'password', :role => 'Admin')
+User.create!(:name => 'Peter Duong', :email => 'petrduong@gmail.com', :password => 'password', :role => 'Admin')
+User.create!(:name => 'Edward Huang', :email => 'shinyenhuang@gmail.com', :password => 'password', :role => 'Admin')
+User.create!(:name => 'Chang Liang', :email => 'changwliang@gmail.com', :password => 'password', :role => 'Admin')
+User.create!(:name => 'Kyle Chang', :email => 'hchang409@gmail.com', :password => 'password', :role => 'Admin')
+User.create!(:name => 'Jerry Uejio', :email => 'jerryuejio@gmail.com', :password => 'password', :role => 'Admin')
+
+User.create!(:name => 'Hatim Khan', :email => 'hatimkhan@berkeley.edu', :password => 'password', :role => 'Admin')
 
 User.create!(:name => 'Molly Zhai', :email => 'mmzhai@berkeley.edu', :password => 'password', :role => 'Admin')
