bc5e18c4e1b3fbbea3b263452808162e790ee525
mmzhai1 <mmzhai@berkeley.edu>
2016-04-01 20:25:10 +0000

    k

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,12 @@
 class GroupsController < ApplicationController
   
-  # before_filter :ensure_sign_in
-  # include SalesforceClient
+  before_filter :ensure_sign_in
+  include SalesforceClient
 
-  # def new
+  def new
   #   # Set filter values for _filter partial
-  #   @filter_values = get_filter_values
-  # end
+    @filter_values = get_filter_values
+  end
   
   def index
   	@groups = Group.where(:user => current_user)
